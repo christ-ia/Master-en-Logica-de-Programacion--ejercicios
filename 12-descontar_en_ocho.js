b46d75342dd91c2be8783eaf374be55e4dbaa808
xchristx <cheisagui@gmail.com>
2022-05-06 00:13:49 -0400

    Ejercicio 16 resuelto

--- 12-descontar_en_ocho.js
+++ 12-descontar_en_ocho.js
@@ -2,10 +2,13 @@ function tablaReductoraEn8 (num){
 
     let list8 = `--- Del ${num} hasta 0 ---\n`;
 
-    for (let i = num ; i >= 0; i-= 8){
+    for (var i = num ; i >= 0; i-= 8){
         list8 += `nº ${i}\n`
     }
+
+    if (num % 8 != 0){list8 += "nº 0\n"}
+
     list8 += '--- FIN ---'
     return list8;
 }
-console.log(tablaReductoraEn8(104))
+console.log(tablaReductoraEn8(109))
